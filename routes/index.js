5269f96bb481589708b9dbcc06d62e2d877de49f
Nicolas Choquet <nicolachoquet06250@gmail.com>
2020-05-27 19:53:52 +0200

    add custom car in VR in page /voiture

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/voiture', function(req, res, next) {
+  fs.readFile(__dirname + '/../public/voiture.html', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.status(200).send(data);
+  });
 });
 
 module.exports = router;
